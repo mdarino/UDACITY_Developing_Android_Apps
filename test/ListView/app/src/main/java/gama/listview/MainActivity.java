90e2b593a87f64cf0619638d8261b4dcdcb672c9
U-ARIMEX2003\mdarino <mdarino@wks-lab-238.arimex.central>
2016-06-13 15:59:03 -0300

    Carpeta:Test -> Test con bluetooth

---
+++ test/ListView/app/src/main/java/gama/listview/MainActivity.java
@@ -0,0 +1,65 @@
+package gama.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] paises = { "Argentina", "Chile", "Paraguay", "Bolivia",
+            "Peru", "Ecuador", "Brasil", "Colombia", "Venezuela", "Uruguay" };
+    private String[] habitantes = { "40000000", "17000000", "6500000",
+            "10000000", "30000000", "14000000", "183000000", "44000000",
+            "29000000", "3500000" };
+    private TextView tv1;
+    private ListView lv1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1=(TextView)findViewById(R.id.tv1);
+        lv1 =(ListView)findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, paises);
+        lv1.setAdapter(adapter);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                tv1.setText("Población de " + lv1.getItemAtPosition(position) + " es " + habitantes[position]);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+/*        if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
